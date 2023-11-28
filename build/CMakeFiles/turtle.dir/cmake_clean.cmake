7367b77a8a5a9008b472b18eb3308c0243848073
Toni <68860474+tkmusic@users.noreply.github.com>
2023-11-28 21:43:09 +0100

    Configured CMake

---
+++ build/CMakeFiles/turtle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turtle.dir/src/main.c.o"
+  "CMakeFiles/turtle.dir/src/main.c.o.d"
+  "turtle"
+  "turtle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+   printf("Hello, this is Turtle!");
+   return 0;
+}
