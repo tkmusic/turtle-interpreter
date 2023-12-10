929d9478fb6dacc685b5a51937f185249e92889b
Toni <68860474+tkmusic@users.noreply.github.com>
2023-12-10 16:38:00 +0100

    Enhanced chunk functionality
    In chunk.c, extend the Chunk struct to include a ValueArray constants for storing constants. Modify initChunk and freeChunk functions to initialize and free this new array, respectively. Introduce addConstant to append values to the constants array and return their indices.
    Update Debugging and Disassembly
    
    In debug.c, update disassembleChunk to handle OP_CONSTANT instructions, calling constantInstruction for detailed constant printing. Define constantInstruction to print information about OP_CONSTANT instructions, including the constant value.
    Usage Example in main.c
    
    In main.c, demonstrate the new functionality by creating a chunk with an OP_CONSTANT instruction and a constant value, followed by an OP_RETURN instruction. Disassemble and print the chunk using disassembleChunk.
    Value Management in value.c and value.h
    
    In value.c and value.h, add functionality to manage a dynamic array of Value types using ValueArray. Functions include initValueArray, writeValueArray, freeValueArray, and printValue.

--- src/debug.c
+++ src/debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk* chunk, const char* name){
     printf("== %s ==\n", name);
@@ -18,6 +19,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+        case OP_CONSTANT:
+        return constantInstruction("OP_CONSTANT", chunk, offset);
       case OP_RETURN:
         return simpleInstruction("OP_RETURN", offset);
       default:
@@ -25,3 +28,11 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         return offset + 1;
     }
 } 
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
